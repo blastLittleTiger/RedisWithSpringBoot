338bb2c542333a883f9f0dc8ded27716f4a95ad8
unknown <zishan2050@gmail.com>
2015-10-18 23:08:35 +0530

    Removed unnecessary configuration

--- SpringRedisCache/src/main/java/com/zishan/spring/SpringRedisCacheApplication.java
+++ SpringRedisCache/src/main/java/com/zishan/spring/SpringRedisCacheApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @EnableCaching
-@EnableMongoRepositories("com.zishan.mongo.repository")
 @SpringBootApplication
 public class SpringRedisCacheApplication {
 
